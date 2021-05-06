135dddb19bb81122b51cb54f53df160dfedfef5e
navedkhan0809 <navedkhan0809@gmail.com>
2021-05-06 18:01:46 +0530

    UC-4 Added

--- Snake and Ladder/src/snake_ladder/Snake_ladder.java
+++ Snake and Ladder/src/snake_ladder/Snake_ladder.java
@@ -5,37 +5,38 @@ public class Snake_ladder {
 	public static void main(String[] args) {
 		
 		int initial=0;
-		System.out.println("For player 1,start position= "+initial);
-		
-		double roll1 = Math.floor((Math.random()*10) % 6)+1;
-		int roll = (int)roll1;
-		System.out.println("number on rolling a die= "+roll);
-
-		double option1 = Math.floor(Math.random()*10) % 3;
-		int option = (int)option1;
-		
-		switch (option) {
-		
-			case 1: {
-			System.out.println("Its a ladder");	
-			initial= initial+roll;
-			break;
-			}
+		while (initial <= 150) {
 			
-			case 2: {
-			System.out.println("Its a snake");
-			initial= initial-roll;
-				if (initial < 0) {
-					initial = 0;
-				}
-			break;
-			}
+			double roll1 = Math.floor((Math.random()*10) % 6)+1;
+			int roll = (int)roll1;
+	
+			double option1 = Math.floor(Math.random()*10) % 3;
+			int option = (int)option1;
 			
-			default:
-			System.out.println("No Play");
-			initial = initial;
+				if (option == 1) {	
+					
+					initial= initial+roll;
+					
+					if (initial == 100) {
+						
+						System.out.println("Player 1 Won the game,position= "+initial);
+						break;
+							
+					} else if (initial > 100){
+						initial=initial-roll;
+							
+						}
+				
+					} else if (option == 2 ) {
+						initial= initial-roll;
+						if (initial < 0) {
+							initial = 0;
+						}
+				
+					} else {
+						//System.out.println("No Play");
+						//System.out.println(initial);
+				    }
+			}
 		}
-		System.out.println("Now position becomes = "+initial);
-	}
-
 }
