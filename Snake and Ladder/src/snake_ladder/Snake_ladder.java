06637c9a71131e6f8e2e9b16cb7c4fcd71d35577
navedkhan0809 <navedkhan0809@gmail.com>
2021-05-06 18:30:53 +0530

    UC-7 Added

--- Snake and Ladder/src/snake_ladder/Snake_ladder.java
+++ Snake and Ladder/src/snake_ladder/Snake_ladder.java
@@ -3,52 +3,81 @@ package snake_ladder;
 public class Snake_ladder {
 
 	public static void main(String[] args) {
+			
+			int initial=0,initial2=0;
+			System.out.println("For player 1,start position= "+initial);
+			System.out.println("For player 2,start position= "+initial2);
+			System.out.println("Lets start playing....");
 		
-		int initial=0,n=0;
-		System.out.println("For player 1,start position= "+initial);
+			while (initial <= 150) {
+				
+				double roll11 = Math.floor((Math.random()*10) % 6)+1;
+				int roll = (int)roll11;
+				
+				double roll12 = Math.floor((Math.random()*10) % 6)+1;
+				int roll2 = (int)roll12;
+				
 		
-		while (initial <= 150) {
-			
-			double roll1 = Math.floor((Math.random()*10) % 6)+1;
-			int roll = (int)roll1;
-			
-			System.out.println("Die roll= "+roll);
-			n++;
-	
-			double option1 = Math.floor(Math.random()*10) % 3;
-			int option = (int)option1;
-			
-				if (option == 1) {
-					
-						System.out.println("Its a ladder");	
-						initial= initial+roll;
+				double option1 = Math.floor(Math.random()*10) % 3;
+				int option = (int)option1;
+				
+				double option12 = Math.floor(Math.random()*10) % 3;
+				int option2 = (int)option12;
+				
+					if (option == 1 && option2 == 1) {
 						
-						System.out.println("Position ="+initial);
-							if (initial == 100) {
+							// Its a ladder condition
+							initial= initial+roll;
+							initial2= initial2+roll2;
+							
+								if (initial > 100) {
+									initial=initial-roll;
+								}
+							
+								if (initial2 > 100) {
+									initial2=initial2-roll2;
+								}
+							
+							// got ladder so rolling dice again
+							roll11 = Math.floor((Math.random()*10) % 6)+1;
+							roll = (int)roll11;
+							roll12 = Math.floor((Math.random()*10) % 6)+1;
+							roll2 = (int)roll12;
+							
+							initial= initial+roll;
+							initial2= initial2+roll2;
+							
+								if (initial > 100) {
+									initial=initial-roll;
+								}
 								
-								System.out.println("Player 1 Won the game,position= "+initial);
-								System.out.println("The number of times the dies was played to win="+n);
+								if (initial2 > 100) {
+									initial2=initial2-roll2;
+								}
+						
+							if (initial == 100) {
+								System.out.println("Player 1 Won the game at position "+initial);
+								System.out.println("And Player 2 position= "+initial2);
 								break;
 								
-							} else if (initial > 100){
-								initial=initial-roll;
+							} else if (initial2 == 100){
+								System.out.println("Player 2 Won the game at position "+initial2);
+								System.out.println("And Player 1 position= "+initial);
+								break;
 							}
-				
-				} else if (option == 2 ) {
 					
-					System.out.println("Its a snake");
-					initial= initial-roll;
-					System.out.println("Position ="+initial);
+					} else if (option == 2 && option2 == 2 ) {
+						// Its a snake
+						initial= initial-roll;
+						initial2= initial2-roll2;
+						
+					if ( initial < 0 && initial2 <0 ) {
+						initial = 0;
+						initial2 = 0;
+					}
 					
-						if (initial < 0) {
-							initial = 0;
-						}
-				
-				} else {
-					System.out.println("No Play");
-					System.out.println("Position ="+initial);
+					}
+	
 				}
-
 			}
-		}
 }
