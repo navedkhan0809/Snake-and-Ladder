71be17d641b862c72edd0a41a1c8fc4bbc5a6ecd
navedkhan0809 <navedkhan0809@gmail.com>
2021-05-06 17:48:19 +0530

    UC-3 Added

--- Snake and Ladder/src/snake_ladder/Snake_ladder.java
+++ Snake and Ladder/src/snake_ladder/Snake_ladder.java
@@ -7,9 +7,35 @@ public class Snake_ladder {
 		int initial=0;
 		System.out.println("For player 1,start position= "+initial);
 		
-		double roll = Math.floor((Math.random()*10) % 6)+1;
+		double roll1 = Math.floor((Math.random()*10) % 6)+1;
+		int roll = (int)roll1;
 		System.out.println("number on rolling a die= "+roll);
 
+		double option1 = Math.floor(Math.random()*10) % 3;
+		int option = (int)option1;
+		
+		switch (option) {
+		
+			case 1: {
+			System.out.println("Its a ladder");	
+			initial= initial+roll;
+			break;
+			}
+			
+			case 2: {
+			System.out.println("Its a snake");
+			initial= initial-roll;
+				if (initial < 0) {
+					initial = 0;
+				}
+			break;
+			}
+			
+			default:
+			System.out.println("No Play");
+			initial = initial;
+		}
+		System.out.println("Now position becomes = "+initial);
 	}
-	
+
 }
