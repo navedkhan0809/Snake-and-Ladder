a8407eb41135d5a38d469a174ba32e4818a43dd1
Owner <Owner@DESKTOP-SB1VGHE>
2021-05-06 17:01:19 +0530

    UC-1 Added for starting position

--- Snake and Ladder/src/snake_ladder/Snake_ladder.java
+++ Snake and Ladder/src/snake_ladder/Snake_ladder.java
@@ -3,7 +3,10 @@ package snake_ladder;
 public class Snake_ladder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake Ladder problem");
+		
+		int initial=0;
+		System.out.println("For player 1,start position= "+initial);
+
 	}
 	
 }
